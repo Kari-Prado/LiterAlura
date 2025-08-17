0dd57facdd187fc3d4651ae952f3de8f544a19a1
Kari-Prado <vitoria28prado@gmail.com>
2025-08-17 11:37:33 -0300

    Update LiterAluraApplication.java

--- src/main/java/com/vitoriaPrado/literalura/LiterAluraApplication.java
+++ src/main/java/com/vitoriaPrado/literalura/LiterAluraApplication.java
@@ -1,15 +1,19 @@
 package com.vitoriaPrado.literalura;
 
+import com.vitoriaPrado.literalura.menu.Menu;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationContext;
 
 @SpringBootApplication
 public class LiterAluraApplication implements CommandLineRunner {
 
-    @Autowired
-    private Menu menu;
+    private final Menu menu;
+
+    public LiterAluraApplication(Menu menu) {
+        this.menu = menu;
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(LiterAluraApplication.class, args);
